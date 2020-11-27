da6943e00376e889ea878265dd81dbca590c1985
Poste <Poste@LAPTOP-A1EQ8EUS>
2020-11-27 08:53:13 -0500

    Hello

---
+++ src/Vecteur.java
@@ -0,0 +1,14 @@
+public class Vecteur {
+
+    private int[] vecteur;
+
+    Vecteur(int nbElements){
+        this.vecteur = new int[nbElements];
+        for (int i = 0; i < nbElements; i++) this.vecteur[i] = 0;
+
+
+    }
+
+
+
+}
